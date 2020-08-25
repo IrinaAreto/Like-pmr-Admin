93c7f99156faf1ac871db67615097f5202934de4
Irina <irinamalkush@gmail.com>
2020-08-25 23:40:19 +0300

    Admin for fast food like

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { PageHeader } from "./components/PageHeader";
+import { NavigationRouters } from "./components/NavigationRouters";
+import { PageFooter } from "./components/PageFooter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="layout">
+        <header className="header">
+          <PageHeader />
+        </header>
+          <NavigationRouters />
+        <footer className="footer">
+          <PageFooter />
+        </footer>
+      </div>
     </div>
   );
 }
